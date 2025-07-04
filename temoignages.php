277abeb1c04cac17f976e3abf397afc330194b0e
ABB-CIEL <habbey480@gmail.com>
2025-07-04 11:34:07 +0200

    commit10

--- temoignages.php
+++ temoignages.php
@@ -64,6 +64,7 @@ if (!is_array($temoignages)) {
     }
   </style>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Témoignages</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
